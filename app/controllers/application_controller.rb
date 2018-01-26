44f21ae14da9969ff05c595c5ed7ecbffb003fc3
CircleCI-NYULib <lib-webservices@nyu.edu>
2018-01-26 16:51:36 -0500

    Add comments so I know waht I was doing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'citero'
+
 class ApplicationController < Sinatra::Base
 
   set :show_exceptions, false
@@ -13,7 +14,7 @@ class ApplicationController < Sinatra::Base
     @calling_system = params[:calling_system] if @@whitelisted_calling_systems.include?(params[:calling_system])
 
     unless missing_params?
-      # require 'pry'; binding.pry
+      # Map the citation and then decide what the course of acion is
       @csf = Citero.map(primo.get_pnx_json).from_pnx_json.send("to_#{@cite_to.to_format}".to_sym)
       push_to_or_download
     else
@@ -27,6 +28,34 @@ class ApplicationController < Sinatra::Base
     erb :error
   end
 
+  # Should we push to an external citation system or download the file?
+  def push_to_or_download
+    @cite_to.push_to_external ? push_to_external : download
+  end
+
+ private
+
+  # Downloads a file with the citation
+  def download
+    content_type @cite_to.mimetype
+    attachment(@cite_to.filename)
+    @csf.force_encoding('UTF-8')
+  end
+
+  # Push to external system either by redirecting with a callback url which
+  # sends the external system back to push_to_cite
+  # Or render the form and post with Javascript, depending on how the
+  # external system API expects it
+  def push_to_external
+    # require 'pry';binding.pry
+    if !params.has_key?(:callback) && @cite_to.redirect
+      redirect @cite_to.action + callback, 303
+    else
+      erb :post_form
+    end
+  end
+
+  # Whitelist available formats and forward to relevant classes
   def push_format(cite_to)
     case cite_to.to_sym
       when :endnote
@@ -43,34 +72,18 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  def push_to_or_download
-    @cite_to.push_to_external ? push_to_external : download
-  end
-
-  # Downloads a file with citation
-  def download
-    content_type @cite_to.mimetype
-    attachment(@cite_to.filename)
-    @csf.force_encoding('UTF-8')
-  end
-
-  def push_to_external
-    # require 'pry';binding.pry
-    if !params.has_key?(:callback) && @cite_to.redirect
-      redirect @cite_to.action + callback, 303
-    else
-      erb :post_form
-    end
-  end
-
+  # This URL that you are currently on is also the callback URL
+  # but in the case of the callback we always want to render, so flag it!
   def callback
     @callback ||= ERB::Util.url_encode("#{request.url}&callback")
   end
 
+  # Require params
   def missing_params?
     !(@institution && @local_id && @cite_to && @calling_system)
   end
 
+  # Make a call to Primo to get the PNX record
   def primo
     @primo ||= CallingSystems::Primo.new(@local_id, @institution)
   end
