b52c2d4f3c5d30652f864c1545f13876190c7346
BAlter <barnaby.alter@nyu.edu>
2018-09-24 16:02:05 -0400

    Begin building out more contorller specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -20,28 +20,52 @@ describe 'ApplicationController' do
   }
 
   describe "GET /:identifier", vcr: true do
-    subject { last_response.body }
     before do
       get "/", params
     end
-    context 'when all required parameters are present' do
-      it { is_expected.to include 'Pinsker, Sanford' }
+
+    describe 'error messages when required params are missing' do
+      subject { last_response.body }
+      context 'when local_id is missing' do
+        let(:local_id) { nil }
+        it { is_expected.to include missing_params_error_message }
+      end
+      context 'when cite_to is missing' do
+        let(:cite_to) { nil }
+        it { is_expected.to include missing_params_error_message }
+      end
+      context 'when institution is missing' do
+        let(:institution) { nil }
+        it { is_expected.to include missing_params_error_message }
+      end
+      context 'when calling_system is missing' do
+        let(:calling_system) { nil }
+        it { is_expected.to include missing_params_error_message }
+      end
     end
-    context 'when local_id is missing' do
-      let(:local_id) { nil }
-      it { is_expected.to include missing_params_error_message }
-    end
-    context 'when cite_to is missing' do
-      let(:cite_to) { nil }
-      it { is_expected.to include missing_params_error_message }
-    end
-    context 'when institution is missing' do
-      let(:institution) { nil }
-      it { is_expected.to include missing_params_error_message }
-    end
-    context 'when calling_system is missing' do
-      let(:calling_system) { nil }
-      it { is_expected.to include missing_params_error_message }
+
+    describe 'responses when all required parameters are present' do
+      subject { last_response }
+      context 'and cite_to is RIS' do
+        let(:cite_to) { 'ris' }
+        its(:body) { is_expected.to include 'Pinsker, Sanford' }
+      end
+      context 'and cite_to is EndNote' do
+        let(:cite_to) { 'endnote' }
+        its(:status) { is_expected.to eql 303 }
+      end
+      context 'and cite_to is BibTex' do
+        let(:cite_to) { 'bibtex' }
+        its(:body) { is_expected.to include 'Pinsker, Sanford' }
+      end
+      context 'and cite_to is RefWorks' do
+        let(:cite_to) { 'refworks' }
+        its(:body) { is_expected.to include 'Pinsker, Sanford' }
+      end
+      context 'and cite_to is OpenURL' do
+        let(:cite_to) { 'openurl' }
+        its(:body) { is_expected.to include 'Sanford' }
+      end
     end
   end
 end
